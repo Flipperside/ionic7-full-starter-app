0cb7bee34738d90e5ee93ba6d68cb3e3b93280aa
Piastr <viktor_kuzmin_99@mail.ru>
2023-12-29 22:14:57 +0300

    finaly

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { FormsComponent } from './forms/forms.component'
+import { ContactCardComponent } from './contact-card/contact-card.component';
+import { SettingsComponent } from "./settings/settings.component"
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule)
+  },
+  {
+    path: 'forms',
+    component: FormsComponent
+  },
+  {
+    path: 'contact-card',
+    component: ContactCardComponent
+  }, {
+    path: 'settings',
+    component: SettingsComponent
+  },
+  {
+    path: 'profil',
+    loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule)
+  },
+  {
+    path: 'forms', 
+    component: FormsComponent
+  },
+  {
+    path: 'filters',
+    loadChildren: () => import('./forms/filters/filters.module').then(m => m.FiltersPageModule)
+  }, 
+  {
+    path: 'validations',
+    loadChildren: () => import('./forms/validations/validations.module').then(m => m.ValidationsPageModule)
+  }
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+
+
+export class AppComponent {
+  public appPages = [
+    { title: 'Inbox', url: '/folder/inbox', icon: 'входящие' },
+    { title: 'outbox', url: '/folder/outbox', icon: 'исходящие' },
+    { title: 'favorites', url: '/folder/favorites', icon: 'понравившиеся' },
+    { title: 'archived', url: '/folder/archived', icon: 'архив' },
+    { title: 'trash', url: '/folder/trash', icon: 'корзина' },
+    { title: 'spam', url: '/folder/spam', icon: 'предупреждения/спам' },
+    // Добавляем поле для перехода на страницу пометок
+    { title: 'notes', url: '/notes', icon: 'Пометки' },
+    { title: 'filters', url:'/filters',icon: 'reader-outline'},
+    { title: 'validations', url: '/validations', icon: '' },  
+    { title: 'privacy-policy', url: '/privacy-policy', icon: 'battery-full-outline' },
+    { title: 'login', url: '/login', icon: '' },
+  ];
+  public labels = ['Семья', 'Друзья', 'Заметки', 'Работа', 'Путешествия', 'Напоминания'];
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+  }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
+}
+
+
+
+
